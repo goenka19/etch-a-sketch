3370f8bc7b07028cbd12895a31b5a371fa2b024e
Ujjwal Goenka <46502186+goenka19@users.noreply.github.com>
2021-06-09 00:31:50 +0530

    Color change on hover added

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 console.log("HELLO");
-
 var container = document.querySelector("#container");
-
 var counter = 1
 for (let i = 1; i <= 16; i++) 
 {
     for (let j = 1; j <= 16; j++)
     {
-        var cell = document.createElement("div");
+        let cell = document.createElement("div");
         cell.classList.add("cells");
-        cell.textContent = counter;
-        
+        //cell.textContent = counter;
+        cell.addEventListener('mouseover', ()=>
+        {
+            cell.style.backgroundColor = 'pink';
+        })
         container.appendChild(cell);
 
         counter ++;
     }
-    
 }
