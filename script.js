5e654b73fc1669d1c438b7e0124cc81eb3a5a492
Ujjwal Goenka <46502186+goenka19@users.noreply.github.com>
2021-06-08 23:10:14 +0530

    added the grid

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("HELLO");
+
+var container = document.querySelector("#container");
+
+var counter = 1
+for (let i = 1; i <= 16; i++) 
+{
+    for (let j = 1; j <= 16; j++)
+    {
+        var cell = document.createElement("div");
+        cell.classList.add("cells");
+        cell.textContent = counter;
+        
+        container.appendChild(cell);
+
+        counter ++;
+    }
+    
+}
