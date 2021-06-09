452386191dd356ff7a19064aa7b1d8b79ff30a5c
Ujjwal Goenka <46502186+goenka19@users.noreply.github.com>
2021-06-09 18:47:13 +0530

    added some shit, messed some other

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
 console.log("HELLO");
-var container = document.querySelector("#container");
-var counter = 1
-for (let i = 1; i <= 16; i++) 
-{
-    for (let j = 1; j <= 16; j++)
-    {
-        let cell = document.createElement("div");
-        cell.classList.add("cells");
-        //cell.textContent = counter;
-        cell.addEventListener('mouseover', ()=>
-        {
-            cell.style.backgroundColor = 'pink';
-        })
-        container.appendChild(cell);
+var container = document.querySelector(".container");
 
-        counter ++;
+var counter = 1
+var size = 16;
+
+// function to create grid
+function grid(size)
+{
+    var container = document.querySelector(".container");
+
+    for (let i = 1; i <= size; i++) 
+    {
+        for (let j = 1; j <= size; j++)
+        {   
+            
+            let cell = document.createElement("div");
+            cell.classList.add("cells");
+            cell.textContent = counter;
+            cell.addEventListener('mouseover', ()=>
+            {
+                cell.style.backgroundColor = 'pink';
+            })
+            container.appendChild(cell);
+
+            counter ++;
+        }
     }
 }
+
+grid(size); // initial grid display
+
+var btn = document.querySelector('#create');
+btn.addEventListener('click', ()=>
+{
+    var size = prompt("Enter the size")
+    console.log(size);
+    let cells = document.querySelectorAll('.cells');
+    container.remove();
+
+    let new_container = document.createElement("div");
+    new_container.classList.add("container");
+    let main = document.querySelector(".main");
+    main.appendChild(new_container);
+    grid(size);
+    
+})
