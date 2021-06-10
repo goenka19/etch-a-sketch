2ee676d27cd29edcb22beae74880db836292c4b8
Ujjwal Goenka <46502186+goenka19@users.noreply.github.com>
2021-06-11 00:34:18 +0530

    fixed the button

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 console.log("HELLO");
 var container = document.querySelector(".container");
 
-var counter = 1
 var size = 16;
+var color1 = 'pink'
 
 // function to create grid
 function grid(size)
 {
+    container.style.gridTemplateColumns = `repeat(${size}, 40px)`;
+    container.style.gridTemplateRows = `repeat(${size}, 40px)`;
     for (let i = 1; i <= size; i++) 
     {
         for (let j = 1; j <= size; j++)
@@ -14,14 +16,12 @@ function grid(size)
             
             let cell = document.createElement("div");
             cell.classList.add("cells");
-            cell.textContent = counter;
             cell.addEventListener('mouseover', ()=>
             {
-                cell.style.backgroundColor = 'pink';
+                cell.style.backgroundColor = color1;
             })
             container.appendChild(cell);
 
-            counter ++;
         }
     }
 }
@@ -30,3 +30,15 @@ grid(size); // initial grid display
 
 var btn = document.querySelector('#create');
 
+btn.addEventListener('click', ()=>
+{
+    var size = prompt("Enter the size")
+    console.log(size);
+    var cells = container.lastElementChild;
+    while(cells)
+    {
+        container.removeChild(cells);
+        cells = container.lastElementChild;
+    }
+    grid(size);
+})
---
+++ taks.md
@@ -0,0 +1,5 @@
+add color picker
+keep size constant chnage the size of the cells
+ability to add numbers for navigation
+ability to download the image
+add limit fo the size of the grid
