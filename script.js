9530de7467e6a7ba82ff6f85aa19d968c0c8c077
Ujjwal Goenka <46502186+goenka19@users.noreply.github.com>
2021-06-11 22:04:23 +0530

    made the grid size constant
    cell size will increase to fit the grid size

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 console.log("HELLO");
 var container = document.querySelector(".container");
-
+var counter =1;
 var size = 16;
 var color1 = 'pink'
+var num = 0;
+
 
 // function to create grid
 function grid(size)
 {
-    container.style.gridTemplateColumns = `repeat(${size}, 40px)`;
-    container.style.gridTemplateRows = `repeat(${size}, 40px)`;
+    container.style.gridTemplateColumns = `repeat(${size}, ${100/size}%)`;
+    container.style.gridTemplateRows = `repeat(${size}, ${100/size}%)`;
     for (let i = 1; i <= size; i++) 
     {
         for (let j = 1; j <= size; j++)
@@ -16,29 +18,49 @@ function grid(size)
             
             let cell = document.createElement("div");
             cell.classList.add("cells");
+            if (num == 1) 
+            {
+                cell.textContent = counter;
+            }
+            
             cell.addEventListener('mouseover', ()=>
             {
                 cell.style.backgroundColor = color1;
             })
             container.appendChild(cell);
-
+            counter++;
         }
     }
+    counter = 1;
 }
 
 grid(size); // initial grid display
 
-var btn = document.querySelector('#create');
+var btncreate = document.querySelector('#create');
 
-btn.addEventListener('click', ()=>
+btncreate.addEventListener('click', ()=>
 {
-    var size = prompt("Enter the size")
+    size = prompt("Enter the size")
     console.log(size);
+    
+    deletegrid();
+    grid(size);
+})
+
+function deletegrid()
+{
     var cells = container.lastElementChild;
     while(cells)
     {
         container.removeChild(cells);
         cells = container.lastElementChild;
     }
+}
+
+var btnnumber = document.querySelector('#number');
+btnnumber.addEventListener('click', ()=>
+{
+    num = 1;
+    deletegrid();
     grid(size);
 })
--- taks.md
+++ taks.md
@@ -1,5 +1,3 @@
 add color picker
-keep size constant chnage the size of the cells
-ability to add numbers for navigation
 ability to download the image
-add limit fo the size of the grid
+add limit for the size of the grid
